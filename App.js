4f141912bc94766e71389acc993098625bbb3963
Christian Malki <christianmalki00@hotmail.com>
2023-08-12 21:17:24 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { FitnessContext } from "./Context";
+import HomeScreen from "./screens/HomeScreen";
+import StackNavigator from "./StackNavigator";
+
+export default function App() {
+  
+  const FitnessCards = async () => {
+    try {
+      const docRef = await addDoc(collection(db, "fitness"), {
+        title: title,
+        isChecked: false,
+      });
+      console.log("Document written with ID: ", docRef.id);
+      setTitle("");
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+
+  return (
+    <FitnessContext>
+      <StackNavigator />
+    </FitnessContext>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
